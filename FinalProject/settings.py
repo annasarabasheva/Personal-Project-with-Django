0e3cfe5b9628cdcf8e52ef0816bf2b8fcc01e874
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-11-12 13:05:01 +0200

    Create individual apps and configure PostgreSQL as database for the project

--- FinalProject/settings.py
+++ FinalProject/settings.py
@@ -30,6 +30,15 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+PROJECT_MADE_APPS = [
+    'FinalProject.common',
+    'FinalProject.accounts',
+    'FinalProject.universities',
+    'FinalProject.students',
+    'FinalProject.forum'
+
+]
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+] + PROJECT_MADE_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -75,9 +84,13 @@ WSGI_APPLICATION = 'FinalProject.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "UniMatch",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
@@ -117,6 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
