48b66ad4bd9ea3e6f1ac9c1461f9e5586fb664ab
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-11-13 15:15:15 +0200

    Extend the User model from Django and visualize Login and Register templates roughly

--- FinalProject/urls.py
+++ FinalProject/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('FinalProject.common.urls')),
+    path('accounts/', include('FinalProject.accounts.urls')),
 ]
