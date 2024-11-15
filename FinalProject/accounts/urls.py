c43a9fcd2ed34efdea053869fa31238946927e60
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-11-15 21:31:44 +0200

    Add template for profile page and render it

--- FinalProject/accounts/urls.py
+++ FinalProject/accounts/urls.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.views import LogoutView
-from django.urls import path
-from FinalProject.accounts.views import AppUserLoginView, AppUserRegisterView
+from django.urls import path, include
+from FinalProject.accounts.views import AppUserLoginView, AppUserRegisterView, my_profile
+
 urlpatterns = [
     path('login/', AppUserLoginView.as_view(), name='login'),
     path('register/', AppUserRegisterView.as_view(), name='register'),
-    path('logout/', LogoutView.as_view(), name='logout')
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('profile/', include([
+        path('', my_profile, name='profile-details'),
+    ]))
 
 ]
--- FinalProject/accounts/views.py
+++ FinalProject/accounts/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth import get_user_model, login
 from django.contrib.auth.views import LoginView
+from django.shortcuts import redirect, get_object_or_404, render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 from FinalProject.accounts.forms import AppUserCreationForm
+from FinalProject.accounts.models import Profile
 
 UserModel = get_user_model()
 
@@ -24,3 +26,15 @@ class AppUserRegisterView(CreateView):
         login(self.request, self.object)  # Auto Login after Register
 
         return response
+
+
+def my_profile(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+
+    profile = get_object_or_404(Profile, user=request.user)
+
+    context = {
+        'profile': profile,
+    }
+    return render(request, 'accounts/profile-details-page.html', context)
