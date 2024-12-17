1b77452333cd87dd24fb6021a15f8f6cd2640c86
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-12-17 16:21:48 +0200

    Add personalized User Activity Summary for logged in user in his/her profile page

--- FinalProject/accounts/views.py
+++ FinalProject/accounts/views.py
@@ -3,12 +3,14 @@ from django.contrib.auth import get_user_model, login, logout
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import Group
 from django.contrib.auth.views import LoginView
+from django.db.models import Count, Sum
 from django.shortcuts import redirect, get_object_or_404, render
 from django.urls import reverse_lazy, reverse
 from django.views import View
 from django.views.generic import CreateView, DetailView, UpdateView
 from FinalProject.accounts.forms import AppUserCreationForm, ProfileEditForm
 from FinalProject.accounts.models import Profile
+from FinalProject.forum.models import Post, Thread
 
 UserModel = get_user_model()
 
@@ -27,10 +29,8 @@ class AppUserRegisterView(CreateView):
     def form_valid(self, form):
         response = super().form_valid(form)
 
-        # Auto-login after registration
         login(self.request, self.object)
 
-        # Update profile `is_student` value
         profile = self.object.profile
         profile.is_student = form.cleaned_data.get('is_student', False)
         profile.save()
@@ -39,11 +39,9 @@ class AppUserRegisterView(CreateView):
             self.object.is_staff = True
             self.object.save()
 
-            # Add the user to the "Student Group"
             student_group, created = Group.objects.get_or_create(name='Student Group')
             self.object.groups.add(student_group)
 
-            # Redirect to student form if the user registers as a student
             return redirect('student-form')
 
         return response
@@ -56,6 +54,18 @@ class ProfileDetailView(LoginRequiredMixin, DetailView):
 
     def get_object(self, queryset=None):
         return get_object_or_404(Profile, user=self.request.user)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+
+        context['thread_count'] = Thread.objects.filter(author=user).count()
+        context['post_count'] = Post.objects.filter(author=user).count()
+        context['likes_received'] = Post.objects.filter(author=user).annotate(
+            like_count=Count('likes')
+        ).aggregate(total_likes=Sum('like_count'))['total_likes'] or 0
+
+        return context
 
 
 class ProfileEditView(LoginRequiredMixin, UpdateView):
